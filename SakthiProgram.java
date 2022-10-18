ccd9100b3aa0abee5b816183202d3015a28fc8d1
sakthinesanramakrishnan <116069022+sakthinesanramakrishnan@users.noreply.github.com>
2022-10-18 06:33:58 -0400

    Add files via upload

---
+++ SakthiProgram.java
@@ -0,0 +1,9 @@
+
+public class SakthiProgram {
+
+	public static void main(String[] args) {
+ System.out.println("my father ramakrishnan thimmureddiyar");
+ System.out.println("my mother nagamani ramakrishnan");
+	}
+
+}
